145df92b7f5ebfa7ab487733bea88329e8a3b769
netosep <clementesepulveda27@gmail.com>
2024-06-26 22:45:03 -0300

    chore: :memo: docs and `me` resource

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,12 +2,40 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UserController extends BaseController
 {
-    public function index()
+    /**
+     * @OA\Get(
+     *   tags={"User"},
+     *   path="/api/user/me",
+     *   summary="User information",
+     *   security={{"bearerAuth":{}}},
+     *   @OA\Response(
+     *     response=200,
+     *     description="User information response",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(property="success", type="boolean", default=true, example=true),
+     *       @OA\Property(property="code", type="integer", example=200),
+     *       @OA\Property(property="result", type="object", ref="#/components/schemas/User")
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=401,
+     *     description="Unauthenticated",
+     *     @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Unauthenticated.")
+     *     )
+     *   )
+     * )
+     */
+    public function me(Request $request)
     {
-        // do something
+        $user = User::with('addresses')->find($request->user()->id);
+
+        return $this->jsonResponse($user);
     }
 }

4870c65e4ac3f9ca206a7ffabcefb0baf38287d4
netosep <clementesepulveda27@gmail.com>
2024-06-28 16:03:11 -0300

    test: :white_check_mark: move used functions to abstract test class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+use App\Models\Address;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    const GUARD_API = 'sanctum';
+
+    use WithFaker;
+    use DatabaseTransactions;
+
+    public function createUser(array $attributes = [], Model $parent = null): User
+    {
+        return User::factory()->create($attributes, $parent);
+    }
+
+    public function createAddress(array $attributes = [], Model $parent = null): Address
+    {
+        return Address::factory()->create($attributes, $parent);
+    }
 }

088552ef8f965c5c85e97e90649eb4c658ad2ed3
netosep <clementesepulveda27@gmail.com>
2024-06-28 19:58:50 -0300

    chore: :sparkles: organizing routes and add new

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,21 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'auth'])->name('auth');
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::controller(AuthController::class)
+    ->group(function () {
+        Route::post('/login', 'auth');
+        Route::post('/register', 'register');
+        Route::post('/logout', 'logout')->middleware('auth:sanctum');
+        Route::post('/user/change-password', 'changePassword')->middleware('auth:sanctum');
+    });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-    Route::controller(UserController::class)
-        ->prefix('/user')
+    Route::prefix('/user')
+        ->controller(UserController::class)
         ->group(function () {
-            Route::get('/', 'index')->name('index');
-            Route::put('/', 'update')->name('update');
-            Route::delete('/', 'destroy')->name('destroy');
+            Route::get('/', 'index');
+            Route::put('/', 'update');
+            Route::delete('/', 'destroy');
 
             Route::apiResource('address', AddressController::class);
         });
